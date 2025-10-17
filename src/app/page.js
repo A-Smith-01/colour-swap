21d2f8aeda0c8b4136a096a2c6a359cd40ec67e3
A-Smith-01 <alex3.141smith@gmail.com>
2025-10-17 14:53:47 +0100

    Changed default page to product page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Link from "next/link"
+import { redirect } from 'next/navigation'
 
 export default function Home(){
+    redirect('/productPage')
     return (<>
         <Link href="/colourConfig">Colour config</Link>
         <Link href="/productPage">Product page</Link>
