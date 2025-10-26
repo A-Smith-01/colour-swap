e9587db2122c9befd2f2fd9582274776b0500318
A-Smith-01 <alex3.141smith@gmail.com>
2025-10-26 17:24:44 +0000

    Refactored Shape component into separate file

--- src/app/productSelection/page.js
+++ src/app/productSelection/page.js
@@ -1,13 +1,14 @@
 'use client'
+import * as THREE from 'three/webgpu'
 import { useState, useRef } from "react"
 import { Canvas, useFrame } from "@react-three/fiber";
-import * as THREE from 'three/webgpu'
 import shapes from "../shapes";
 import Image from "next/image";
 import Link from "next/link"
 import styles from "./page.module.css";
 import colours from "../colours";
 import ColourSelector from "../_components/ColourSelector";
+import Shape from "../_components/Shape";
 
 
 export default function Main(){
@@ -185,29 +186,3 @@ function ColourFilter({colours, handleClick, selected}){
     )
 }
 
-function Shape({colour="#ffffff", shape}){
-  const colourValue = new THREE.Color(parseInt ( colour.replace("#","0x"), 16 ));
-  const ref = useRef()
-  useFrame((state, delta) => {
-    ref.current.rotation.x += delta;
-    ref.current.rotation.y += delta;
-  })
-
-  function renderSwitch() {
-    switch(shape){
-        case 'cube':
-            return <boxGeometry args={[3,3,3]}/>
-        case 'tetrahedron':
-            return <tetrahedronGeometry attach="geometry" args={[3,0]}/>
-        case 'torus':
-            return <torusGeometry args={[2,0.66,15,15]} />
-    }
-  }
-
-  return (
-    <mesh ref={ref}> 
-        {renderSwitch()}
-        <meshStandardMaterial color={colourValue}/>
-    </mesh>
-  )
-}
